93b061c0e2eed1c02d5d95a094e2c7fe613c1357
Mattias Karlsson <mattias.karlsson13@learnet.se>
2021-05-10 12:42:52 +0200

    second

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
+
 // INIT WebsocketServer
 const WebSocket = require('ws');
 const Port = 80;
+
 const wsServer = new WebSocket.Server({
    port: Port
 });
@@ -14,18 +16,31 @@ const startTransaction = JSON.stringify(start);
 const responsJSON = require("./BootNotification.json");
 const respond = JSON.stringify(responsJSON);
 
-// Handeling requests from Clients.
-wsServer.on('connection', function (socket) {
+let stationarray = [];
+                                                                                    // Handeling requests from Clients.
+wsServer.on('connection', function (socket, req) {
 
     console.log('A client just connected');
+    console.log(req.url)
 
     socket.on('message', function(msg) {
+       getCS(); 
         
         let parsedMsg = JSON.parse(msg);
         console.log(parsedMsg);
 
         switch(parsedMsg[2]){
             case'BootNotification':
+
+            let station = new chargingStation(
+                parsedMsg[3].chargingStation.serialNumber,
+                 parsedMsg[3].chargingStation.model,
+                  parsedMsg[3].chargingStation.vendorName,
+                   parsedMsg[3].chargingStation.firmwareVersion);
+            
+                                                                                    //Sparar alla "BootNotification" i en array
+            stationarray.push(station);
+
             console.log('Its a BootNotification');
             break;
 
@@ -46,12 +61,37 @@ wsServer.on('connection', function (socket) {
                 console.log('Avslutar laddning');
             }
             break;
+            
 
-        }
-
+        };
+                                                         //Startar laddning från annan klient.
+         if(parsedMsg==1){
+             wsServer.clients.forEach(function(client){
+                 console.log("Skickar ut Start charging")
+              client.send(startTransaction)
+            })
+         };
     });
     socket.send(respond)
-})
+});
+
+
+ class chargingStation {
+    constructor(serialNumber, model, vendorName, firmwareVersion) {
+        this.serialNumber = serialNumber;
+        this.model = model;
+        this.vendorName = vendorName;
+        this.firmwareVersion = firmwareVersion;
+    }
+} 
+
+async function getCS(){
+    fetch('https://csmsapi2021.azurewebsites.net/api/ChargingPoints')
+  .then(response => response.json())
+  .then(data => console.log(data));
+}
+
+
 
 
  //wsServer.clients.forEach(function(client){
